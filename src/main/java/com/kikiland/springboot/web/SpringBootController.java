4b4f92d8c64209adab1f0dc27c960ed8adfa86fc
kiki <1527737879@qq.com>
2020-07-18 22:15:25 +0800

    创建工程

---
+++ src/main/java/com/kikiland/springboot/web/SpringBootController.java
@@ -0,0 +1,19 @@
+package com.kikiland.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 王雅琦
+ * 2020/07/18
+ */
+@Controller
+public class SpringBootController {
+
+    @RequestMapping("/hello")
+    public @ResponseBody
+    Object hello() {
+        return "Hello World";
+    }
+}
